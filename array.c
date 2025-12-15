e152f30ed9cbb0732f03f5201b19642385afbdd0
Tashif <officialtashif@gmail.com>
2025-12-15 08:53:33 +0530

    memory allocation for array

--- array.c
+++ array.c
@@ -25,10 +25,7 @@ void push(Array *arr, int element)
         arr->capacity = arr->capacity + 5;
     }
 
-    
-    
     arr->data[arr->currentIndex++] = element;
-    
 }
 
 void printArr(Array *arr)
@@ -40,6 +37,37 @@ void printArr(Array *arr)
     printf("Done!\n");
 }
 
+void insert(Array *arr, int index, int element)
+{
+    if (index < 0 || index > arr->currentIndex)
+    {
+        printf("Invalid index!");
+        return;
+    }
+    if (arr->capacity == arr->currentIndex)
+    {
+        int *temp = realloc(arr->data, sizeof(int) * (arr->capacity + 5));
+
+        if (!temp)
+        {
+            printf("Could not insert at the specified index!\n");
+            return;
+        }
+
+        arr->data = temp;
+        arr->capacity = arr->capacity + 5;
+    }
+
+    for (int i = arr->currentIndex; i > index; i--)
+    {
+        arr->data[i] = arr->data[i - 1];
+    }
+
+    arr->data[index] = element;
+
+    arr->currentIndex++;
+}
+
 int main()
 {
 
@@ -47,13 +75,9 @@ int main()
     push(&students, 90);
     push(&students, 100);
     push(&students, 87);
-    push(&students, 96);
-    push(&students, 45);
-    push(&students, 98);
-    push(&students, 12);
-    push(&students, 15);
-    push(&students, 98);
-    push(&students, 19);
+    
+    insert(&students, 1, 120);
+    insert(&students, 0, 987);
 
     printArr(&students);
     free(students.data);
